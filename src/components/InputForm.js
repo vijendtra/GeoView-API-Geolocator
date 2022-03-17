c7c9258df4abd524eb3b9795031393b35ed2017b
vijendra yadav <vy@vijendras-MacBook-Pro.local>
2022-03-17 06:42:10 -0400

    adding more

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,6 +3,8 @@ import * as cheerio from "cheerio";
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { Box } from "@material-ui/core";
+import {get} from "cheerio/lib/api/traversing";
 
 let provinces = [];
 // eslint-disable-next-line no-unused-vars
@@ -19,15 +21,51 @@ async function getProvince() {
     console.error(error);
   }
 }
-getProvince().then(() => console.log(`Got the final provinces: ${provinces}`));
-export default function ComboBox() {
+getProvince().then(() => console.log(`All provinces : ${provinces}`));
+
+
+let concises = [];
+// eslint-disable-next-line no-unused-vars
+async function getConcise() {
+  try {
+    const result = await geogratisAPI.get("/codes/concise");
+    const $ = cheerio.load(result.data);
+    $("body > table > tbody > tr").each((index, element) => {
+      let check = $($(element).find("td")[1]).text();
+      check = check.replace(/\r?\n|\r/g, " ").replace(/ /g, "");
+      concises.push(check);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getConcise().then(() => console.log(`All concises : ${concises}`));
+
+export default function ControllableStates() {
+  const [value, setValue] = React.useState("");
+  const [inputValue, setInputValue] = React.useState("");
+
   return (
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={provinces}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Province" />}
-    />
+    <div style={{ paddingLeft: 100 }}>
+      <Box height={50} />
+      <Autocomplete
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        id="controllable-states-demo"
+        options={provinces}
+        sx={{ width: 250 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Select Province" />
+        )}
+      />
+    </div>
+
   );
 }
