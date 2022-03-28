5d4b23f0ec9a83cd417ea747c19f32b7d25b3b31
vijendra yadav <vy@vijendras-MacBook-Pro.local>
2022-03-28 07:31:07 -0400

    changes

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,85 +1,78 @@
 import geogratisAPI from "../api/geogratisAPI.js";
-import * as cheerio from "cheerio";
+//import * as cheerio from "cheerio";
 import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import { Box } from "@material-ui/core";
+//import { useState } from "react";
+import { Autocomplete, Box, TextField } from "@mui/material";
 //import { get } from "cheerio/lib/api/traversing";
 
-let provinces = [];
 // eslint-disable-next-line no-unused-vars
-async function getProvince() {
+let geoData = [];
+async function getgeoName(searchValue) {
+  let searchItem = searchValue.concat("*");
   try {
-    const result = await geogratisAPI.get("/codes/province");
-    const $ = cheerio.load(result.data);
-    $("body > table > tbody > tr").each((index, element) => {
-      let check = $($(element).find("td")[2]).text();
-      check = check.replace(/\r?\n|\r/g, " ").replace(/ /g, "");
-      provinces.push(check);
+    const result = await geogratisAPI.get("/geonames.json", {
+      params: {
+        q: searchItem,
+        num: 20,
+      },
+    });
+    let jsonObject = JSON.stringify(result);
+    console.log(JSON.parse(jsonObject).data.items);
+    let itemArray = JSON.parse(jsonObject).data.items;
+    itemArray.forEach((element) => {
+      if (element.location != null) {
+        let finalValue =
+          element.name +
+          "," +
+          element.location +
+          "," +
+          element.concise.code +
+          "," +
+          element.province.code;
+
+        geoData.push(finalValue);
+        console.log(finalValue);
+      }
     });
   } catch (error) {
     console.error(error);
   }
 }
-getProvince().then(() => console.log(`All provinces : ${provinces}`));
-
-let concises = [];
 // eslint-disable-next-line no-unused-vars
-async function getConcise() {
-  try {
-    const result = await geogratisAPI.get("/codes/concise");
-    const $ = cheerio.load(result.data);
-    $("body > table > tbody > tr").each((index, element) => {
-      let check = $($(element).find("td")[1]).text();
-      check = check.replace(/\r?\n|\r/g, " ").replace(/ /g, "");
-      var result = check.split("-");
-      concises.push(result[1]);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getConcise().then(() => console.log(`All concises : ${concises}`));
-
-export default function ControllableStates() {
-  const [value, setValue] = React.useState("");
-  const [inputValue, setInputValue] = React.useState("");
+function InputForm() {
+  const [name, setName] = React.useState();
+  const handleChange = (event) => {
+    if (geoData.length !== 0) {
+      geoData = [];
+    }
+    setName(event.target.value);
+    getgeoName(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
-    <div style={{ paddingLeft: 100 }}>
-      <Box height={14} />
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
       <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={provinces}
-        sx={{ width: 250 }}
+        options={geoData}
         renderInput={(params) => (
-          <TextField {...params} label="Select Province" />
+          <TextField
+            {...params}
+            id="outlined-name"
+            label="GeoName"
+            value={name}
+            onChange={handleChange}
+          />
         )}
       />
-      <Box height={14} />
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={concises}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Type" />}
-      />
-    </div>
+    </Box>
   );
 }
+
+export default InputForm;
